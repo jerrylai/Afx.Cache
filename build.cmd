3a7813810bafc22dfd9f8537dfc9381c06c8e3c1
jerry <jerrylai@aliyun.com>
2026-04-21 13:44:57 +0800

    add .net 8.0

--- build.cmd
+++ build.cmd
@@ -1,7 +1,8 @@
 @echo off
 set Build="%SYSTEMDRIVE%\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\MsBuild.exe"
 if exist publish rd /s /q publish
-dotnet build "NETStandard2.0/Afx.Cache/Afx.Cache.csproj" -c Release 
+# dotnet build "NETStandard2.0/Afx.Cache/Afx.Cache.csproj" -c Release
+dotnet build "NET8.0/Afx.Cache/Afx.Cache.csproj" -c Release
 cd publish
 del /q/s *.pdb
 del /q/s Microsoft*
